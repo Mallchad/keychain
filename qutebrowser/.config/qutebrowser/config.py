fea656074b614879ca4ec42ef8c8b4be6179e5da
Mallchad <ncaprisunfan@gmail.com>
2021-05-12 15:43:15 +0100

    Addition: Enable darkmode by default

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,4 +1,7 @@
 config.load_autoconfig()
 config.set("colors.webpage.darkmode.enabled", True)
 config.bind(",darkmode", "config-cycle colors.webpage.darkmode.enabled")
+config.set("colors.webpage.darkmode.enabled", True)
+config.set("colors.webpage.darkmode.threshold.background", 155)
+config.set("colors.webpage.darkmode.threshold.text", 155)
 config.set("auto_save.session", True)
