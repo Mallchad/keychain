6eb24459d0ad6eae807888a45b98853e91e79c1a
Mallchad <ncaprisunfan@gmail.com>
2021-05-13 12:06:30 +0100

    Adittion: Bound Chromium-like commands for inspector and tabs

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,9 +1,14 @@
 config.load_autoconfig()
-config.set("colors.webpage.darkmode.enabled", True)
-config.bind(",darkmode", "config-cycle colors.webpage.darkmode.enabled")
+# Qutebrowser Bindings
 config.bind("b", "set-cmd-text --space :tab-select  ", mode="normal")
 config.bind("<Ctrl-Shift-s>", "open qute://back;;tab-prev")
+# Chromium Like Bindings
+config.bind("<ctrl+tab>", "tab-next")
+config.bind("<ctrl+shift+tab>", "tab-prev")
+config.bind("<f12>", "devtools")
+# Misc Config
 config.bind(",test", "message-info testmessg")
+# Misc Settings
 config.set("colors.webpage.darkmode.enabled", True)
 config.set("colors.webpage.darkmode.threshold.background", 155)
 config.set("colors.webpage.darkmode.threshold.text", 155)
