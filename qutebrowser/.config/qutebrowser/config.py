dc354988046158c5d27a7ffb444526ea3bcc9203
mallchad <ncaprisunfan@gmail.com>
2021-07-07 11:39:50 +0100

    [qutebrowser] Change: Tweaked hint settings and appearance to be easier to use

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -43,6 +43,47 @@ config.set("input.insert_mode.auto_load", False)     # When focusing a prompt af
 config.set("input.insert_mode.leave_on_load", True)
 # Don't enter insert mode when focusing a plugin like flash
 config.set("input.insert_mode.plugins", False)
+
+# Hints Settings
+# Increase the minimum character count to make the behaviour more deterministic
+# normally, the maximum hints is 24, on for each character,
+# For a normal page, this limit can easily be exceeded, which quickly introduces
+# secondary characters to input, its difficult to determine at a glance if you
+# are looking at a page with 2 chars
+# This makes it difficult to mentally prepare, do I prepare to type 1 character, or 2?
+#
+# By making the minimum charactrs to type 2, you end up with 24*24 hint options,
+# more than enough for the vast majority of pages, meaning you almost always expect 2
+# characters
+#
+# The different hint character set is also reduced to the home row by default
+# 9 characters, not 24, this leaves you a mere 9 for 1 character and 9*9 for 2
+# characters 9*9 is still a lot of hints, but it is common enough and uncomfortable
+# enough to type to warrent increasing availible characters
+#
+# There is also this quirky behaviour where the hints are "scattered", so they hint
+# characters are distnct from close by hints
+# Whilst this makes sense in theory, increasing the distinctiveness of each hint pair
+# to reduce confusion
+# There is reason to believe that actually you can better mentally prepare yourself if
+# you know beforehand that your first chartarget will be in the upper alphabeta,
+# for example
+# Also, if you lose track of the hint location, you will immediately see nearby hints
+# with the same first sequence
+#
+# Additionally, by reducing the hint size and using uppercase characters,
+# the hint can be made easier to view and find
+# A background change to a darker colour can make it less jaring
+# and focus on the text, not the background colour
+config.set("hints.min_chars", 2)
+config.set("hints.chars", "abcdefghijklmnopqrstuvwxyz")
+config.set("hints.scatter", False)
+# config.set("fonts.hints", "bold 9 default_family")
+config.set("hints.uppercase", True)
+config.set("colors.hints.fg", "rgba(255, 255, 255)")
+config.set("colors.hints.bg", "rgba(29,29,98,0.85)")
+config.set("colors.hints.match.fg", "red")
+config.set("hints.radius", 2)           # Reduce rounding radius to make box sharper
 # Misc Settings
 config.set("auto_save.session", True)
 config.set("content.autoplay", False)
